8db5847a2addc795c78bb87a8b24611b79ce9c06
iwnlMski <mski.python@gmail.com>
2021-02-08 14:05:06 +0100

    Create Bundle and Task models and migrate them

--- main_page/models.py
+++ main_page/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
+
 # Create your models here.
+class Bundle(models.Model):
+    name = models.CharField(max_length=256, unique=True)
+    created_date = models.DateField(auto_now_add=True)
+    # tasks_done = models.IntegerField(default=0)
+    # tasks_in_progress = models.IntegerField(default=0)
+    # tasks_to_do = models.IntegerField(default=0)
+
+
+class Task(models.Model):
+    name_of_bundle = models.ForeignKey(Bundle, on_delete=models.CASCADE)
+    task_to_be_done = models.CharField(max_length=256)
--- todo_project/settings.py
+++ todo_project/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'main_page.apps.MainPageConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
