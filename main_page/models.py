63c5c4d64e9171bb306a1afc65eb4c0e6613f77c
iwnlMski <mski.python@gmail.com>
2021-02-08 14:58:17 +0100

    Add __str__ methods to Bundle and Task models

--- main_page/models.py
+++ main_page/models.py
@@ -5,11 +5,17 @@ from django.db import models
 class Bundle(models.Model):
     name = models.CharField(max_length=256, unique=True)
     created_date = models.DateTimeField('date of creation')
+
     # tasks_done = models.IntegerField(default=0)
     # tasks_in_progress = models.IntegerField(default=0)
     # tasks_to_do = models.IntegerField(default=0)
+    def __str__(self):
+        return self.name
 
 
 class Task(models.Model):
     name_of_bundle = models.ForeignKey(Bundle, on_delete=models.CASCADE)
     task_to_be_done = models.CharField(max_length=256)
+
+    def __str__(self):
+        return self.task_to_be_done
